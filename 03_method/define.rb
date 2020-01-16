01a04edb7c5f63e49938b62c39ff73a0cd50425a
Makoto Chiba <hypermkt@gmail.com>
2020-01-17 01:08:36 +0900

    途中までをコミット

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -2,6 +2,12 @@
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
 
+class A1
+  define_method :"//" do
+    "//"
+  end
+end
+
 # Q2.
 # 次の動作をする A2 class を実装する
 # - 1. "SmartHR Dev Team"と返すdev_teamメソッドが存在すること
@@ -9,6 +15,21 @@
 # - 2で定義するメソッドは下記とする
 #   - 受け取った引数の回数分、メソッド名を繰り返した文字列を返すこと
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
+
+class A2
+  def initialize(names)
+    names.each do |name|
+      method_name = "hoge_#{name}"
+      define_singleton_method(method_name) do |num|
+        num.nil? ? dev_team : method_name * num
+      end
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+end
 
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
