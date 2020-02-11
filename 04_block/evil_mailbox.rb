9e327b4ad6520ed77ac3d91e3bd0dfd2b17ef508
Makoto Chiba <hypermkt@gmail.com>
2020-02-11 13:35:19 +0900

    できたところまで

--- 04_block/evil_mailbox.rb
+++ 04_block/evil_mailbox.rb
@@ -3,7 +3,7 @@
 # 基本機能
 # 1. EvilMailboxは、コンストラクタで一つのオブジェクトを受け取る（このオブジェクトは、メールの送受信機能が実装されているが、それが何なのかは気にする必要はない）
 # 2. EvilMailboxは、メールを送るメソッド `send_mail` を持ち、引数として宛先の文字列、本文の文字列を受け取る。結果の如何に関わらず、メソッドはnilをかえす。
-# 3. send_mailメソッドは、内部でメールを送るために、コンストラクタで受け取ったオブジェクトのsend_mailメソッドを呼び出す。このときのシグネチャは同じである。また、このメソッドはメールの送信が成功したか失敗したかをbool値で返す。
+# 3. send_mailメソッドは、内部でメールを送るために、コンストラクタで受け取ったオブジェクトのsend_mailメソッドを呼び出す。このときのシグネチャは同じである。 また、このメソッドはメールの送信が成功したか失敗したかをbool値で返す。
 # 4. EvilMailboxは、メールを受信するメソッド `receive_mail` を持つ
 # 5. receive_mailメソッドは、メールを受信するためにコンストラクタで受け取ったオブジェクトのreceive_mailメソッドを呼び出す。このオブジェクトのreceive_mailは、送信者と本文の2つの要素をもつ配列で返す。
 # 6. receive_mailメソッドは、受け取ったメールを送信者と本文の2つの要素をもつ配列として返す
@@ -17,3 +17,24 @@
 # 邪悪な機能
 # 1. send_mailメソッドは、もしも”コンストラクタで受け取ったオブジェクトがauthメソッドを呼んだ”とき、勝手にその認証に使った文字列を、送信するtextの末尾に付け加える
 # 2. つまり、コンストラクタが第2引数に文字列を受け取った時、その文字列はオブジェクト内に保存されないが、send_mailを呼び出したときにこっそりと勝手に送信される
+
+# TODO: 邪悪な機能が未対応
+class EvilMailBox
+  def initialize(obj, secret_string = nil)
+    obj.auth(secret_string) unless secret_string.nil?
+    @obj = obj
+  end
+
+  def send_mail(to, body, &block)
+    @to = to
+    @body = body
+    res = @obj.send_mail(to, body)
+    block.call(res) if block_given?
+    nil
+  end
+
+  def receive_mail
+    @obj.receive_mail
+    [@to, @body]
+  end
+end
